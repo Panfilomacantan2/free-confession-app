95b136ee318bb8190957d798509f37fddc97f2c3
panfilo macantan <olifnapnatnacam27.com>
2024-07-12 16:36:31 +0800

    node_modules changex

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
 	console.log(`${req.method} ${req.url}`);
 
-    next();
+	next();
 });
 
 app.use('/posts', require('./routes/posts'));
